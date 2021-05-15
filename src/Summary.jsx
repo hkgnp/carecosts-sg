0017d14634d48ef01d8207f122dbc175d93d54f5
hkgnp <ben@pngs.cc>
2021-05-16 01:59:58 +0800

    Added summary page

--- src/Summary.jsx
+++ src/Summary.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Summary = (props) => {
+  const score = props.getTotalCount();
+
+  const calculateCosts = (score) => {
+    if (score < 7) {
+      return <p>$500</p>;
+    } else if (score < 25) {
+      return <p>$1000</p>;
+    } else if (score < 49) {
+      return <p>$2000</p>;
+    } else {
+      return <p>$5000</p>;
+    }
+  };
+
   return (
-    <div className="block bg-gray-400 rounded-lg p-5">
+    <div className="block bg-blue-400 rounded-lg p-5">
       <h1>Summary</h1>
-      {props.getTotalCount()}
+      <p className="mb-5">
+        Thank you for taking the time to do the quick survey. An estimate of
+        your estimated costs are as follows:
+      </p>
+      <p className="mb-5">{calculateCosts(score)}</p>
+      <p>
+        As this is meant to be an estimate, do speak to your Medical Social
+        Worker for further details. If you do not have one, please ask your
+        doctor to refer you to one.
+      </p>
     </div>
   );
 };
