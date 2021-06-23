b0df5ff352fc0c6ac49e7fae98c4ea02da97da91
hkgnp <ben@pngs.cc>
2021-06-23 16:45:33 +0800

    Fixed uneven costs btn sizing issue

--- src/Item.jsx
+++ src/Item.jsx
@@ -3,14 +3,14 @@ import React, { Fragment } from 'react';
 const Item = (props) => {
   return (
     <div className="flex justify-between items-center block bg-blue-600 py-5 px-4 w-full text-white mb-1 shadow-xxl">
-      <div className="flex-col items-center">
+      <div className="flex-col items-center w-3/4">
         <p className="text-xs my-2">
           <span className="rounded-full bg-blue-400 py-1 px-5">
             {props.frequency}
           </span>
         </p>
         <p>{props.item}</p>
-        <p className="text-xs whitespace-pre-wrap">
+        <p className="text-xs ">
           {props.description.split('\\r').map((value, index) => {
             return (
               <Fragment key={index}>
